fd8f5991e9785705eec539dd9e424358f13b17be
KKDD41 <ekaterina.dul@gmail.com>
2022-10-22 12:51:03 +0400

    TimeProvider optimization

---
+++ providers/__init__.py
@@ -0,0 +1,10 @@
+from time_provider import TimeProvider
+from user_provider import UserProvider
+
+
+__all__ = ("TimeProvider", "UserProvider")
+# import os
+# import sys
+# fpath = os.path.join(os.path.dirname("C:\\Users\\Kate\\Desktop\\IRISKA\\Irirska-TelegramChatBot\\providers"), 'providers')
+# sys.path.append(fpath)
+# print(sys.path)
